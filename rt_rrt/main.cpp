81ff465ceb59344409769c8d5fd37c7b9282abf9
xielong <xielong@zju.edu.cn>
2017-12-14 04:46:42 +0800

    Initial Commit

---
+++ rt_rrt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rt_rrt
+//
+//  Created by 谢龙 on 2017/12/14.
+//  Copyright © 2017年 long. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
